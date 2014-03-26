4569a5e2c378ff14c212f5d6c619e6eced5f16f6
Tine Kleivane <kleivane@gmail.com>
2014-03-26 07:59:08 +0100

    Added grunt connect and grunt watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,35 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		connect: {
+			server: {
+		      options: {
+		        port: 9090
+		      }
+    		}
+		},
+
 		open: {
 			server: {
 				path: 'http://localhost:9090/app'
 			}
+		},
+
+		watch: {
+			scripts: {
+    			files: ['app/*']
+			}
 		}
+
 
 
 	});
 
 	grunt.loadNpmTasks('grunt-open');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('server', ['open']);
+	grunt.registerTask('server', ['connect', 'open', 'watch']);
 
 
 }
