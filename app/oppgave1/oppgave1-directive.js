c4744f515859c9f4a5bba8e3cd9c807fd580680a
Tine Kleivane <kleivane@gmail.com>
2014-04-24 17:10:09 +0200

    Hoveddelen av oppgave 1 beskrevet

--- app/oppgave1/oppgave1-directive.js
+++ app/oppgave1/oppgave1-directive.js
@@ -1,9 +1,17 @@
 angular.module('Oppgave1')
-	.directive('ngGithub', function(){
+	.directive('ngGithub', ['$http', function($http){
 		return {
 			restrict: 'E',
-			template: '<a href=""><span>Stargazers: </span>',
+			template: '<a href="{{browse}}">Stargazers: <span class="stargazers">{{count}}</span></a>',
 			replace: true,
-			controller: 'oppgave1-controller'
+			link: function(scope, element, attrs){
+				var user = attrs.user;
+				var repo = attrs.repo;
+				scope.browse = 'https://github.com/'+user+'/'+repo;
+				$http.jsonp('https://api.github.com/repos/'+user+'/'+repo+'?callback=JSON_CALLBACK')
+					.success(function(data){
+						scope.count = data.data.stargazers_count;
+					});
+			}
 		}
-	});
+	}]);
