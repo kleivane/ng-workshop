9bc001c84f4ea5c0e16394d61953ad8436c5cb9d
Tine Kleivane <kleivane@gmail.com>
2014-05-27 15:54:56 +0200

    Fjernet test-linje

--- app/oppgave-scope/scope-directive.js
+++ app/oppgave-scope/scope-directive.js
@@ -9,7 +9,6 @@ angular.module('Scopeoppgave').directive('bookingDirektiv', function() {
     link: function(scope) {
       scope.rompris = 100;
       scope.frokostpris = 25;
-      scope.test = scope.frokost;
     },
     templateUrl: '/app/oppgave-scope/scopetemplate.html'
   };
