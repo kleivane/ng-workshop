a646ba978f84b3d2f478e56f8c6fca9ac72100c9
Tine Kleivane <kleivane@gmail.com>
2014-06-05 20:35:18 +0200

    Stjerneformet er ferdig

--- app/oppgave-form/form-directive.js
+++ app/oppgave-form/form-directive.js
@@ -5,12 +5,21 @@ angular.module('Formoppgave')
 				var maxRating = 5; 
 				$scope.max = maxRating;
 				$scope.starRatings = [];
-				_.each(_.range(1, 6) , function(i){
+				_.each(_.range(0, maxRating) , function(i){
 					$scope.starRatings.push({
-						value: i,
+						value: i+1,
 						filled: false
 					});
 				});
+
+				$scope.rate = function(star){
+					_.each(_.range(0, star.value) , function(i){
+						$scope.starRatings[i].filled = true;
+					});
+					_.each(_.range(star.value, maxRating), function(i){
+						$scope.starRatings[i].filled = false;
+					})
+				};
 			},
 			templateUrl: '/app/oppgave-form/form-template.html'
 		};	
