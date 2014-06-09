8e846f260bb7dfffc1c075b45b004117fd858318
Tine Kleivane <kleivane@gmail.com>
2014-06-09 13:34:45 +0200

    Siste runde med tester

--- app/oppgave-form/form-directive_test.js
+++ app/oppgave-form/form-directive_test.js
@@ -38,6 +38,13 @@ describe('Rating-direktivet', function(){
 		expect($(elm).find('input + i')).not.toHaveClass('fa-star-o')
 	});
 
+	it('skal ha 3 fylt og 2 ufylte stjerneikoner etter å ha klikket på #3', function(){
+		createElm('<span rating></span>');
+		$(elm).find('input[value=3]').click()
+		expect($(elm).find('input:lt(3) + i')).toHaveClass('fa-star');
+		expect($(elm).find('input:gt(2) + i')).toHaveClass('fa-star-o');
+	});
+
 
 
 });
