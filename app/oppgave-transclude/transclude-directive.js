8c830688a7a2d640fd3b613c383f1a49fcf54ecc
Tine Kleivane <kleivane@gmail.com>
2014-06-10 21:34:15 +0200

    Lagt til meny på alle oppgavesidene. Lagt til visning av direktiv i oppgave 1.

--- app/oppgave-transclude/transclude-directive.js
+++ app/oppgave-transclude/transclude-directive.js
@@ -13,8 +13,8 @@ angular.module('Oppgavetransclude').directive('menyDirektiv', function() {
         	'<p>Oppgaver</p>' +
 			'<ul>' +
 				'<li><a href="#template">Template</a></li>' +
-				'<li><a href="#textarea">Textarea</a></li>'+
 				'<li><a href="#scope">Scope</a></li>' +
+        '<li><a href="#textarea">Textarea</a></li>'+
 				'<li><a href="#form">Form</a></li>'+
 				'<li><a href="#transclude">Transclude</a></li>' +
 			'</ul>' +
