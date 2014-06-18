3f87c815d2e5a49a890cf9f758c355629ffda3ab
Tine Kleivane <kleivane@gmail.com>
2014-06-18 20:31:00 +0200

    elm var allerede et jquery-wrappet element

--- app/oppgave-form/form-directive_test.js
+++ app/oppgave-form/form-directive_test.js
@@ -18,33 +18,33 @@ describe('Rating-direktivet', function(){
 
 	it('skal tegne opp likt antall inputfelter som det er maks av', function(){
 		createElm('<span rating></span>');
-		expect($(elm).find('input').length).toBe(5);
+		expect(elm.find('input').length).toBe(5);
 	});
 
 	it('skal ha 5 ufylte stjerneikoner', function(){
 		createElm('<span rating></span>');
-		expect($(elm).find('.fa-star-o').length).toBe(5);
+		expect(elm.find('.fa-star-o').length).toBe(5);
 	});
 
 	it('skal ha 1 fylt og restene ufylte stjerneikoner etter å ha klikket på #1', function(){
 		createElm('<span rating></span>');
-		$(elm).find('input[value=1]').click()
-		expect($(elm).find('input[value=1] + i')).toHaveClass('fa-star')
-		expect($(elm).find('input[value!=1] + i')).not.toHaveClass('fa-star')
+		elm.find('input[value=1]').click()
+		expect(elm.find('input[value=1] + i')).toHaveClass('fa-star')
+		expect(elm.find('input[value!=1] + i')).not.toHaveClass('fa-star')
 	});
 
 	it('skal ha 5 fylt og ingen ufylte stjerneikoner etter å ha klikket på #5', function(){
 		createElm('<span rating></span>');
-		$(elm).find('input[value=5]').click()
-		expect($(elm).find('input + i')).toHaveClass('fa-star')
-		expect($(elm).find('input + i')).not.toHaveClass('fa-star-o')
+		elm.find('input[value=5]').click()
+		expect(elm.find('input + i')).toHaveClass('fa-star')
+		expect(elm.find('input + i')).not.toHaveClass('fa-star-o')
 	});
 
 	it('skal ha 3 fylt og 2 ufylte stjerneikoner etter å ha klikket på #3', function(){
 		createElm('<span rating></span>');
-		$(elm).find('input[value=3]').click()
-		expect($(elm).find('input:lt(3) + i')).toHaveClass('fa-star');
-		expect($(elm).find('input:gt(2) + i')).toHaveClass('fa-star-o');
+		elm.find('input[value=3]').click()
+		expect(elm.find('input:lt(3) + i')).toHaveClass('fa-star');
+		expect(elm.find('input:gt(2) + i')).toHaveClass('fa-star-o');
 	});
 
 
